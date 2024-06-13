273bd21a789728d2ad8e5532adc9424d778cb084
Aksel Skauge Mellbye <aksel.mellbye@silabs.com>
2024-06-13 17:49:40 +0200

    experiment: blinky with si7021
    Signed-off-by: Aksel Skauge Mellbye <aksel.mellbye@silabs.com>

--- samples/basic/blinky/src/main.c
+++ samples/basic/blinky/src/main.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/sensor.h>
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
@@ -24,6 +25,9 @@ int main(void)
 {
 	int ret;
 	bool led_state = true;
+	const struct device *const dev = DEVICE_DT_GET_ANY(silabs_si7006);
+	struct sensor_value temp = {0};
+	struct sensor_value hum = {0};
 
 	if (!gpio_is_ready_dt(&led)) {
 		return 0;
@@ -34,6 +38,13 @@ int main(void)
 		return 0;
 	}
 
+	if (!device_is_ready(dev)) {
+		printk("Device %s is not ready\n", dev->name);
+		return 0;
+	}
+
+	printk("device is %p, name is %s\n", dev, dev->name);
+
 	while (1) {
 		ret = gpio_pin_toggle_dt(&led);
 		if (ret < 0) {
@@ -43,6 +54,24 @@ int main(void)
 		led_state = !led_state;
 		printf("LED state: %s\n", led_state ? "ON" : "OFF");
 		k_msleep(SLEEP_TIME_MS);
+
+		ret = sensor_sample_fetch(dev);
+		if (ret < 0) {
+			printk("Failed sample fetch\n");
+		}
+
+		ret = sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+		if (ret < 0) {
+			printk("Failed channel get\n");
+		}
+		ret = sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &hum);
+		if (ret < 0) {
+			printk("Failed channel get\n");
+		}
+		printf("temp %llu mdeg C, hum %llu m%% RH\n",
+			sensor_value_to_milli(&temp),
+			sensor_value_to_milli(&hum));
+
 	}
 	return 0;
 }
