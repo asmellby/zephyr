5f52b584997517579c1f747bbf257a31f88a4d01
Aksel Skauge Mellbye <aksel.mellbye@silabs.com>
2025-03-28 11:45:16 +0100

    boards: silabs: Add renode simulation support for xG24
    xG24 supports simulation using Renode. Add renode scripts to enable use
    of west simulate and west robot on xG24 boards.
    
    Signed-off-by: Aksel Skauge Mellbye <aksel.mellbye@silabs.com>

--- boards/silabs/dev_kits/xg24_dk2601b/board.cmake
+++ boards/silabs/dev_kits/xg24_dk2601b/board.cmake
@@ -3,3 +3,9 @@
 
 board_runner_args(jlink "--device=EFR32MG24BxxxF1536" "--reset-after-load")
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+
+set(SUPPORTED_EMU_PLATFORMS renode)
+set(RENODE_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/support/xg24_dk2601b.resc)
+set(RENODE_UART "sysbus.usart0")
+include(${ZEPHYR_BASE}/boards/common/renode.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/renode_robot.board.cmake)
--- boards/silabs/radio_boards/xg24_rb4187c/board.cmake
+++ boards/silabs/radio_boards/xg24_rb4187c/board.cmake
@@ -5,3 +5,9 @@ include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 
 board_runner_args(openocd)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+
+set(SUPPORTED_EMU_PLATFORMS renode)
+set(RENODE_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/support/xg24_rb4187c.resc)
+set(RENODE_UART "sysbus.usart0")
+include(${ZEPHYR_BASE}/boards/common/renode.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/renode_robot.board.cmake)
